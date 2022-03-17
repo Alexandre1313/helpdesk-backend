2c5b797619ee0e5266e53bef21ce4552b36c8ee3
Alexandre1313 <alexandre.13a@gmail.com>
2022-03-17 11:47:45 -0300

    tipos enumerados criados

---
+++ src/main/java/com/alexandre/helpdesk/domain/enums/Profile.java
@@ -0,0 +1,35 @@
+package com.alexandre.helpdesk.domain.enums;
+
+public enum Profile {
+	
+	ADMIN(0, "ROLE_ADMIN"), CLIENTE(1, "ROLE_CLIENTE"), TECNICO(1, "ROLE_TECNICO");
+	
+	private Integer code;
+	private String description;
+	
+	private Profile(Integer code, String description) {
+		this.code = code;
+		this.description = description;
+	}
+
+	public Integer getCode() {
+		return code;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+	
+	public static Profile toEnum(Integer code) {
+		if(code == null) {
+			return null;
+		}
+		for(Profile p: Profile.values()) {
+			if(code.equals(p.getCode())) {
+				return p;
+			}
+		}
+		throw new IllegalArgumentException("Perfil inválido!");
+	}
+	
+}
---
+++ src/main/java/com/alexandre/helpdesk/domain/enums/Status.java
@@ -0,0 +1,35 @@
+package com.alexandre.helpdesk.domain.enums;
+
+public enum Status {
+	
+	ABERTO(0, "ABERTO"), ANDAMENTO(1, "ANDAMENTO"), ENCERRADO(1, "ENCERRADO");
+	
+	private Integer code;
+	private String description;
+	
+	private Status(Integer code, String description) {
+		this.code = code;
+		this.description = description;
+	}
+
+	public Integer getCode() {
+		return code;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+	
+	public static Status toEnum(Integer code) {
+		if(code == null) {
+			return null;
+		}
+		for(Status p: Status.values()) {
+			if(code.equals(p.getCode())) {
+				return p;
+			}
+		}
+		throw new IllegalArgumentException("Status inválido!");
+	}
+	
+}
