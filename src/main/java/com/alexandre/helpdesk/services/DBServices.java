f76f1cc95f5b528f15fa4281cba744bdabeb8569
Alexandre1313 <alexandre.13a@gmail.com>
2022-03-17 17:26:24 -0300

    perfil de desenvolvimento

--- src/main/java/com/alexandre/helpdesk/services/DBServices.java
+++ src/main/java/com/alexandre/helpdesk/services/DBServices.java
@@ -31,13 +31,83 @@ public class DBServices {
 		technicianOne.addProfiles(Profile.ADMIN);
 		
 		Client clientOne = new Client(null, "Linus Torvald", "03113040957",
-				"torvalds@gmail.com", "123");
+				"torvalds@gmail.com", "12356");
 		
-		Called called = new Called(null, Priority.MEDIA, Status.ANDAMENTO,
+		Called calledOne = new Called(null, Priority.MEDIA, Status.ANDAMENTO,
 				"Chamado 01", "Primeiro chamado", technicianOne, clientOne);
 		
-		technicianRepository.saveAll(Arrays.asList(technicianOne));
-		clientRepository.saveAll(Arrays.asList(clientOne));
-		calledRepository.saveAll(Arrays.asList(called));
+		
+		Technician	technicianTwo = new Technician(null, "Paulo Gouvea da Costa",
+				"87739562874", "pgouvea@gmail.com", "5893");
+		technicianOne.addProfiles(Profile.TECNICO);
+		
+		Client clientTwo = new Client(null, "Pedro Goulart", "97202482622",
+				"pg@gmail.com", "7458");
+		
+		Called calledTwo = new Called(null, Priority.BAIXA, Status.ABERTO,
+				"Chamado 02", "Segundo chamado", technicianTwo, clientTwo);
+		
+		
+		Technician	technicianThree = new Technician(null, "Ambrósio Pinto da Cruz",
+				"54114095861", "ambrosino@gmail.com", "3340");
+		technicianOne.addProfiles(Profile.TECNICO);
+		
+		Client clientThree = new Client(null, "Carlos Pereira Gomes", "69214454299",
+				"cpg@gmail.com", "334596");
+		
+		Called calledThree = new Called(null, Priority.ALTA, Status.ENCERRADO,
+				"Chamado 03", "Terceiro chamado", technicianThree, clientThree);
+		
+		
+		Technician	technicianFour = new Technician(null, "Ancelmo Nunes Barreto",
+				"79652831310", "anc.nunes@gmail.com", "999865");
+		technicianOne.addProfiles(Profile.TECNICO);
+		
+		Client clientFour = new Client(null, "Gabriel Pereira Matos", "26403981307",
+				"gaby.per.matos@gmail.com", "332587");
+		
+		Called calledFour = new Called(null, Priority.MEDIA, Status.ANDAMENTO,
+				"Chamado 04", "Quarto chamado", technicianFour, clientFour);
+		
+		
+		Technician	technicianFive = new Technician(null, "Mauricio Calabrez",
+				"35811220812", "maurici@gmail.com", "123589745");
+		technicianOne.addProfiles(Profile.TECNICO);
+		
+		Client clientFive = new Client(null, "Teles da Siva Peixoto", "49202526419",
+				"telespeix@gmail.com", "49863");
+		
+		Called calledFive = new Called(null, Priority.ALTA, Status.ABERTO,
+				"Chamado 05", "Quinto chamado", technicianFive, clientFive);
+		
+		
+		Technician	technicianSix = new Technician(null, "Andrade Benetton",
+				"26185786940", "andrade.b@gmail.com", "4125896");
+		technicianOne.addProfiles(Profile.TECNICO);
+		
+		Client clientSix = new Client(null, "Larissa Monterey", "26286457500",
+				"larissa.jk@gmail.com", "3365879");
+		
+		Called calledSix = new Called(null, Priority.MEDIA, Status.ANDAMENTO,
+				"Chamado 06", "Sexto chamado", technicianSix, clientSix);
+		
+		
+		Technician	technicianSeven = new Technician(null, "Osvaldo Lindberg",
+				"35768744886", "osvald.lind@gmail.com", "9998656985");
+		technicianOne.addProfiles(Profile.TECNICO);
+		
+		Client clientSeven = new Client(null, "Abel da luz", "96666702328",
+				"abel.hgf@gmail.com", "123321");
+		
+		Called calledSeven = new Called(null, Priority.MEDIA, Status.ANDAMENTO,
+				"Chamado 07", "Sétimo chamado", technicianSeven, clientSeven);
+		
+		
+		technicianRepository.saveAll(Arrays.asList(technicianOne, technicianTwo, technicianThree, 
+				technicianFour, technicianFive, technicianSix, technicianSeven));
+		clientRepository.saveAll(Arrays.asList(clientOne, clientTwo, clientThree, 
+				clientFour, clientFive, clientSix, clientSeven));
+		calledRepository.saveAll(Arrays.asList(calledOne, calledTwo, calledThree, 
+				calledFour, calledFive, calledSix, calledSeven));
 	}
 }
---
+++ src/main/java/com/alexandre/helpdesk/testconfig/devConfig.java
@@ -0,0 +1,28 @@
+package com.alexandre.helpdesk.testconfig;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.alexandre.helpdesk.services.DBServices;
+
+@Configuration
+@Profile("dev")
+public class devConfig {
+	
+	@Autowired
+	private DBServices dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String value;
+	
+	@Bean
+	public boolean instantiateDB() {
+		if(value.equals("create")){
+			this.dbService.instantiateDb();
+		}
+		return false;
+	}
+}
